ce0630953ce363c888b17fece4c1046e13052280
Asus <Asus@Asus>
2021-05-30 21:06:56 +0300

    Добавление класса Bullet.

---
+++ bullet.py
@@ -0,0 +1,30 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    """Класс для управления снарядами, выпущенными кораблём."""
+
+    def __init__(self, ai_game):
+        """Создаёт объект снарядов в текущей позиции корабля."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+        """Создание снаряда в позиции (0, 0) и перемещается в верхний край 
+        корабля"""
+        self.rect = pygame.rect(0, 0, self.settings.screen_width,
+                                self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+        self.y = float(self.rect.y)
+
+    def update(self):
+        """Перемещает снаряд вверх по экрану."""
+        """Обновление позиции снаряда в вещественном формате."""
+        self.y -= self.settings.bullet_speed
+        """Обновление позиции  прямоугольника."""
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Вывод снаряда на экран."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
