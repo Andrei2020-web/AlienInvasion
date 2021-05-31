502942e0d07c133f9d11ad838c7ae1a776a345c3
Asus <Asus@Asus>
2021-05-31 13:50:28 +0300

    Добавлена стрельба.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion():
@@ -21,19 +22,23 @@ class AlienInvasion():
         self.bg_color = self.settings.bg_color
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+
 
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _update_screen(self):
         """Обновляет изображение на экране и отображает новый экран."""
         self.screen.fill(self.bg_color)
         self.ship.blitme()
-
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
 
     def _check_events(self):
@@ -60,7 +65,13 @@ class AlienInvasion():
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
+    def _fire_bullet(self):
+        """Создание нового снаряда и включение его группу bullets."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
 if __name__ == '__main__':
     """Создаём экземпляр и стартуем игру."""
