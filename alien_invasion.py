721497555a40e95a715152b2030637c371121280
Asus <Asus@Asus>
2021-06-01 16:40:06 +0300

    Перемещение флота пришельцев влево\право и вниз.

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,6 +34,7 @@ class AlienInvasion():
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _update_bullets(self):
@@ -43,6 +44,12 @@ class AlienInvasion():
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """Проверяет, достиг ли флот края экрана, с последующим обновлением
+        позиций всех пришельцев во флоте."""
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _update_screen(self):
         """Обновляет изображение на экране и отображает новый экран."""
@@ -114,6 +121,19 @@ class AlienInvasion():
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """Реагирует на достижение пришельцем края экрана."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Опускает весь флот и меняет направление флота."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
 
 if __name__ == '__main__':
