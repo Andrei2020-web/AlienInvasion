06e09903e2325ac5fad879ac4c389c489ac421ff
Asus <Asus@Asus>
2021-06-01 13:24:34 +0300

    Добавлена возможность создания флота пришельцев.

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion():
@@ -23,7 +24,9 @@ class AlienInvasion():
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
 
+        self.create_fleet()
 
     def run_game(self):
         """Запуск основного цикла игры."""
@@ -47,6 +50,7 @@ class AlienInvasion():
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         pygame.display.flip()
 
     def _check_events(self):
@@ -81,6 +85,36 @@ class AlienInvasion():
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def create_fleet(self):
+        """Создание флота вторжения."""
+        """Создание пришельца и вычисление количества пришельцев в ряду.
+        Интервал между соседними пришельцами равен ширене пришельца."""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        """Определение кол-ва рядов, помещающихся на экране."""
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height)
+                             - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        """Создание флота."""
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """Создание пришельца и размещение его в ряду."""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
+
 
 if __name__ == '__main__':
     """Создаём экземпляр и стартуем игру."""
