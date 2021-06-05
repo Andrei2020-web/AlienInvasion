4ff195ba3f2a1d15d3eb30264bb33987fe07a112
Asus <Asus@Asus>
2021-06-05 18:18:12 +0300

    Добавлена инициализация панели результатов, начисления очков за сбитых пришельцев с учетом изменения динамики игры и вывод счета на экран.

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ from ship import Ship
 from bullet import Bullet
 from alien import Alien
 from button import Button
+from scoreboard import Scoreboard
 
 
 class AlienInvasion():
@@ -22,11 +23,14 @@ class AlienInvasion():
         self.settings.screen_height = self.screen.get_rect().height
 
         pygame.display.set_caption("Инопланетное вторжение")
+
         """Назначение цвета фона"""
         self.bg_color = self.settings.bg_color
 
-        """Создание экземпляра для хранения игровой статистики."""
+        """Создание экземпляра для хранения игровой статистики 
+        и панели результатов."""
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -64,6 +68,11 @@ class AlienInvasion():
         """При обнаружении попадания удалить снаряд и пришельца."""
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens,
                                                 True, True)
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+
         if not self.aliens:
             """Cоздание нового флота."""
             self._create_fleet()
@@ -89,6 +98,10 @@ class AlienInvasion():
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        """Вывод информации о счете."""
+        self.sb.show_score()
+
         """Кнопка 'Играть' отображается в том случае, если игра неактивна."""
         if not self.stats.game_active:
             self.play_button.draw_button()
@@ -112,9 +125,13 @@ class AlienInvasion():
         """Запускает новую игру при нажатии кнопки 'Играть'."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            """Сброс динамики игры."""
+            self.settings.initialize_dynamic_settings()
+
             """Сброс игровой статистики."""
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
 
             """Очистка списков пришельцев и снарядов."""
             self.aliens.empty()
